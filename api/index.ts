e79a04ed892b57c2424f93ab1b0ca421fdf68224
STierProgrammer <sabasajaia40@gmail.com>
2024-12-08 01:26:43 +0400

    Set up MongoDB and Express Server

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello, World!");
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import express from 'express';
+
+dotenv.config();
+
+const mongoUri = process.env.MONGO;
+
+if (!mongoUri) 
+  throw new Error('MONGO environment variable is not defined');
+
+mongoose.connect(mongoUri)
+    .then(() => {
+        console.log("Connected to MongoDB!");
+    })
+    .catch((err) => {
+        console.log("Error connecting to DB: " + err);
+    });
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(5018, () => {
+    console.log("Server is running on port 5018!");
+});
